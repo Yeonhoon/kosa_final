69b3e6330f946d74c1fa6fb3b811772e995437a2
yeonhoon <aidenjang94@gmail.com>
2021-06-01 18:02:31 +0900

    'flask_0601'

--- posenet/web/server.py
+++ posenet/web/server.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import Flask, jsonify, render_template, request, session, redirect
 import pandas as pd
-import user_mgmt
+import user_mgmt as um
 # from control import user_mgmt as um
 app = Flask(__name__)
 
@@ -13,13 +13,22 @@ def index():
 def signUpform():
     return render_template('user/signup.html')
 
+@app.route('/checkMid', methods=['POST'])
+def checkId():
+    user = um.User()
+    data = request.form['check_id'] # 키로 받아야 함. 그래야 value를 사용 가능
+    if user.checkId(data) is None:
+        return jsonify(result='success')
+    else:
+        return jsonify(result='failed')
+
 @app.route('/signup', methods=['POST'])
 def signUp():
     user_id = request.form('mid')
     user_email = request.form('mmeail')
     user_pw = request.form('mpw')
     user_name = request.form('mname')
-    user_mgmt.signup(user_id, user_email, user_pw, user_name)
+    um.signup(user_id, user_email, user_pw, user_name)
     return redirect('/')
 
 if __name__ == '__main__':
--- posenet/web/user_mgmt.py
+++ posenet/web/user_mgmt.py
@@ -4,11 +4,11 @@ orcl_dsn = cx_Oracle.makedsn(host='localhost', port=1521, sid='orcl')
 conn = cx_Oracle.connect(dsn = orcl_dsn, user='jyhoon94', password='Zpflrjs94')
 
 class User:
-    def __init__(self, user_id, user_email, user_pw, user_name):
-        self.user_id = user_id
-        self.user_email = user_email
-        self.user_pw = user_pw
-        self.user_name = user_name
+    # def __init__(self, user_id, user_email, user_pw, user_name):
+    #     self.user_id = user_id
+    #     self.user_email = user_email
+    #     self.user_pw = user_pw
+    #     self.user_name = user_name
 
     def signUp(self, user_id, user_email, user_pw, user_name):
         cursor = conn.cursor()
@@ -16,3 +16,13 @@ class User:
                 (:user_id, :user_email, :user_pw, :user_name)"""
         cursor.execute(sql, (user_id, user_email, user_pw, user_name))
         conn.commit()
+    
+    def checkId(self, user_id):
+        cursor = conn.cursor()
+        sql = """
+            select user_id from user_info where user_id=:user_id
+            """
+        cursor.execute(sql, {"user_id":user_id})
+        result = cursor.fetchone()
+
+        return result
