b8ea3c0dbeb2967e727f131afd834f02b94aa7b5
wnsdud412 <75961476+wnsdud412@users.noreply.github.com>
2021-06-04 09:20:51 +0900

    달력 웹

--- posenet/web/server.py
+++ posenet/web/server.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import re
 from flask import Flask, jsonify, render_template, request, session, redirect
 import pandas as pd
 import user_mgmt as um
@@ -77,29 +78,59 @@ def dash_page():
     # sets = x[0][1]
     # reps = x[0][2]
     # dates = x[0][3]
-
-
-    return render_template('main.html', id=id)
+    con = um.conn
+    knee_query = """select user_id, set_count, rep_count, squat_date from squat_archive where user_id = '{}'""".format(session['user_id'])
+    knee_df = pd.read_sql(knee_query, con=con)
+    # print("df: ", df)
+    knee_fig = px.line(knee_df, x='SQUAT_DATE', y='REP_COUNT', template="simple_white") #barmode='group'
+    knee_graphJSON = json.dumps(knee_fig, cls=plotly.utils.PlotlyJSONEncoder)
+    
+    #정해진 달 (6월) 스쿼트한 날 찾기
+    return render_template('main.html', id=id,test_graphJSON=knee_graphJSON)
 
 # @app.route('/mypage')
 # def my_page():
 #     return render_template('mypage.html')
 
 
-# @app.route('/main')
+# @app.route('/test')
 # def notdash():
-#     con = um.conn
-#     cursor = con.cursor()
-#     query = """select user_id, set_count, rep_count, squat_date from squat_archive where user_id=:user_id"""
-#     df = pd.read_sql(query, con=con)
-#     print("df: ", df)
-#     fig = px.bar(df, x='SET_COUNT', y='REP_COUNT', color='SQUAT_DATE',
-#             template="simple_white") #barmode='group'
-#     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-#     # print(graphJSON)
-#     return render_template('test.html', graphJSON=graphJSON)
+#     return render_template('test.html')
 
+@app.route('/squartdays',methods=['POST'])
+def squartdays():
+    year = request.form['year']
+    month = request.form['month']
+    con = um.conn
+    days_query="""select DISTINCT to_char(squat_date,'DD') as day FROM squat_archive
+                where user_id='{}' and to_char(squat_date,'YYYY')='{}'
+                and to_char(squat_date,'MM')='{:02d}'
+                """.format(session['user_id'],year,int(month))
+    days_df = pd.read_sql(days_query, con=con)
+    days_list=list(map(int,days_df["DAY"].tolist()))
+    days=",".join(map(str,days_list))
+    
+
+    return jsonify(result=days)
+
+@app.route('/kneeAngleGraph',methods=['POST'])
+def kneeAngleGraph():
+    year = request.form['year']
+    month = request.form['month']
+    date = request.form['date']
+    if date[-1]=="★":
+        date=date[:-1]
+    con = um.conn
+    knee_query = """select angles, frame from knee_angle
+                    where user_id='{}' and to_char(angle_date,'YYYY-MM-DD')='{}-{:02d}-{:02d}'
+                    ORDER BY frame""".format(session['user_id'],year,int(month),int(date))
+    knee_df = pd.read_sql(knee_query, con=con)
+    knee_fig = px.line(knee_df, x='FRAME', y='ANGLES', template="simple_white") #barmode='group'
+    knee_graphJSON = json.dumps(knee_fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return knee_graphJSON
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
     
